a07a4d1b3a6f5b8f09e107791638b6fe6e96dc94
louislautz <louislautz@gmx.net>
2023-02-16 15:10:10 +0200

    Started with Gallery view

---
+++ Project/dist/js/gallery.js
@@ -0,0 +1,44 @@
+var slideIndex = 0;
+var slideshows = Array.from(document.getElementsByClassName("slideshow-container"));
+console.log(slideshows)
+
+for (i = 0; i < slideshows.length; i++) {
+    //set custom data attribute to first current image index
+    slideshows[i].setAttribute("data-currentslide", slideIndex);
+    showImage(parseInt(slideshows[i].getAttribute("data-currentslide")), i);
+}
+
+function showImage(imageIndex, slideShowIndex) {
+    var slides = Array.from(slideshows[slideShowIndex].getElementsByClassName("slide"));
+    var counter = slideshows[slideShowIndex].getElementsByClassName("image-counter")[0];
+
+    
+    if (imageIndex > slides.length - 1){
+        imageIndex = 0;
+    }
+    
+    if (imageIndex < 0){
+        imageIndex = slides.length - 1;
+    }
+
+    slides.forEach((element, index) => {
+        if (index == imageIndex) {
+            element.className += " active";
+        } else {
+            element.classList.remove("active");
+        }
+    });
+    slideshows[slideShowIndex].setAttribute("data-currentslide", imageIndex);
+    counter.innerHTML = `${imageIndex + 1}/${slides.length}`
+}
+
+
+function prevImage(element) {
+    const slideShowIndex = slideshows.indexOf(element.parentElement.parentElement);
+    showImage(parseInt(slideshows[slideShowIndex].getAttribute("data-currentslide")) - 1, slideShowIndex)
+}
+
+function nextImage(element) {
+    const slideShowIndex = slideshows.indexOf(element.parentElement.parentElement);
+    showImage(parseInt(slideshows[slideShowIndex].getAttribute("data-currentslide")) + 1, slideShowIndex)
+}
